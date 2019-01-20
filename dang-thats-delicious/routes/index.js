07d44035c0b6d588554b3fac046d08ff2fe2eda7
Jarrod Davis <developer@jarrodldavis.com>
2019-01-20 13:07:04 -0700

    Video 07
    Core Concept - Controllers and the MVC Pattern

--- dang-thats-delicious/routes/index.js
+++ dang-thats-delicious/routes/index.js
@@ -1,20 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-// Do work here
-router.get('/', (req, res) => {
-  const jarrod = { name: 'Jarrod', age: 24, cool: true };
-  // res.send('Hey! It works!');
-  // res.json(jarrod);
-  // res.json(req.query);
-  jarrod.dog = req.query.dog
-  jarrod.title = 'I love food'
-  res.render('hello', jarrod);
-});
+const storeController = require('../controllers/storeController');
 
-router.get('/reverse/:name', (req, res) => {
-  const reverse = [...req.params.name].reverse().join('');
-  res.send(reverse);
-})
+// Do work here
+router.get('/', storeController.homePage);
 
 module.exports = router;
