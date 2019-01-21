021ecd793ecc2ef55685327e22f1a07707943180
Jarrod Davis <developer@jarrodldavis.com>
2019-01-21 13:19:07 -0700

    Video 18
    Uploading and Resizing Images with Middleware

--- dang-thats-delicious/controllers/storeController.js
+++ dang-thats-delicious/controllers/storeController.js
@@ -1,12 +1,43 @@
 const mongoose = require('mongoose');
 const Store = mongoose.model('Store');
 
+const multer = require('multer');
+const multerOptions = {
+  storage: multer.memoryStorage(),
+  fileFilter(req, file, next) {
+    if (file.mimetype.startsWith('image/')) {
+      next(null, true);
+    } else {
+      next({ message: 'That filetype is not allowed' }, false);
+    }
+  }
+}
+
+const jimp = require('jimp');
+const uuid = require('uuid');
+
 exports.homePage = (req, res) => {
   res.render('index');
 };
 
 exports.addStore = (req, res) => {
   res.render('editStore', { title: 'Add Store' });
+}
+
+exports.upload = multer(multerOptions).single('photo');
+
+exports.resize = async (req, res, next) => {
+  if (!req.file) {
+    return next();
+  }
+
+  const extension = req.file.mimetype.split('/')[1];
+  req.body.photo = `${uuid.v4()}.${extension}`;
+
+  const photo = await jimp.read(req.file.buffer);
+  await photo.resize(800, jimp.AUTO);
+  await photo.write(`./public/uploads/${req.body.photo}`);
+  next();
 }
 
 exports.createStore = async (req, res) => {
